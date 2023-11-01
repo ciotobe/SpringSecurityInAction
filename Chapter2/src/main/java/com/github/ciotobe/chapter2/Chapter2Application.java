c0addbdc5675c28ef16010efdd71feb141d1bfde
ciotobe <101615554+ciotobe@users.noreply.github.com>
2023-11-01 20:29:46 +0800

    init commit

---
+++ Chapter2/src/main/java/com/github/ciotobe/chapter2/Chapter2Application.java
@@ -0,0 +1,13 @@
+package com.github.ciotobe.chapter2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter2Application.class, args);
+    }
+
+}
