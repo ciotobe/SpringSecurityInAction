7d1e73fec4c1f45fcc53fc37a5765bc0a3d98625
ciotobe <101615554+ciotobe@users.noreply.github.com>
2023-11-02 17:29:30 +0800

    2.3.2 重写端点授权配置 - permitAll()

--- chapter2/src/main/java/com/github/ciotobe/ssia/chapter2/config/ProjectConfig.java
+++ chapter2/src/main/java/com/github/ciotobe/ssia/chapter2/config/ProjectConfig.java
@@ -2,6 +2,8 @@ package com.github.ciotobe.ssia.chapter2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -10,7 +12,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SuppressWarnings("deprecation")
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public UserDetailsService userDetailsService() {
 		var userDetailsService = new InMemoryUserDetailsManager();
@@ -28,5 +30,14 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+    
+    protected void configure(HttpSecurity http) throws Exception{
+    	http.httpBasic();
+    	
+    	http.authorizeRequests()
+    		.anyRequest()
+    		//.authenticated();
+    		.permitAll();
     }
 }
