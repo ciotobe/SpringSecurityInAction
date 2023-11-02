b3c166b25b46abe83a15cee1c55063aaa4897cb2
ciotobe <101615554+ciotobe@users.noreply.github.com>
2023-11-02 18:42:53 +0800

    2.3.3 以不同方式设置配置 - 代码清单2.10

--- chapter2/src/main/java/com/github/ciotobe/ssia/chapter2/config/ProjectConfig.java
+++ chapter2/src/main/java/com/github/ciotobe/ssia/chapter2/config/ProjectConfig.java
@@ -1,36 +1,28 @@
 package com.github.ciotobe.ssia.chapter2.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SuppressWarnings("deprecation")
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
-	@Bean
-	public UserDetailsService userDetailsService() {
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 		var userDetailsService = new InMemoryUserDetailsManager();
 		
 		var user = User.withUsername("john")
-				.password("12345")
-				.authorities("read")
-				.build();
+					   .password("12345")
+					   .authorities("read")
+					   .build();
 		
 		userDetailsService.createUser(user);
 		
-		return userDetailsService;
+		auth.userDetailsService(userDetailsService).passwordEncoder(NoOpPasswordEncoder.getInstance());
 	}
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
     
     protected void configure(HttpSecurity http) throws Exception{
     	http.httpBasic();
