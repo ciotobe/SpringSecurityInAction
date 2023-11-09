559eca4e892c482bd112e577007c08c2da1a058c
ciotobe <101615554+ciotobe@users.noreply.github.com>
2023-11-09 23:34:19 +0800

    7 配置权限：限制访问 - 代码清单7.3 - 代码清单7.4 - 代码清单7.5

---
+++ Chapter7/src/test/java/com/github/ciotobe/ssia/chapter7/Chapter7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ciotobe.ssia.chapter7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
