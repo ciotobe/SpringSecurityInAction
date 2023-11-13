823ecbbce32e7b879cc82a8af5ac80317dc73343
ciotobe <101615554+ciotobe@users.noreply.github.com>
2023-11-13 23:01:14 +0800

    9 实现过滤器 - 代码清单9.5 - 代码清单9.6

--- Chapter9/src/main/java/com/github/ciotobe/ssia/chapter9/config/ProjectConfig.java
+++ Chapter9/src/main/java/com/github/ciotobe/ssia/chapter9/config/ProjectConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+import com.github.ciotobe.ssia.chapter9.filter.AuthenticationLoggingFilter;
 import com.github.ciotobe.ssia.chapter9.filter.RequestValidationFilter;
 
 @SuppressWarnings("deprecation")
@@ -13,6 +14,7 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		http.addFilterBefore(new RequestValidationFilter(), BasicAuthenticationFilter.class)
+			.addFilterAfter(new AuthenticationLoggingFilter(), BasicAuthenticationFilter.class)
 			.authorizeRequests()
 			.anyRequest()
 			.permitAll();
