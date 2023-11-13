badcbdf433a9d02706a3769e1db5b1360f1f8c0a
ciotobe <101615554+ciotobe@users.noreply.github.com>
2023-11-13 23:22:46 +0800

    9 实现过滤器 - 代码清单9.7 - 代码清单9.8

--- Chapter9/src/main/java/com/github/ciotobe/ssia/chapter9/config/ProjectConfig.java
+++ Chapter9/src/main/java/com/github/ciotobe/ssia/chapter9/config/ProjectConfig.java
@@ -1,5 +1,6 @@
 package com.github.ciotobe.ssia.chapter9.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -7,14 +8,17 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 
 import com.github.ciotobe.ssia.chapter9.filter.AuthenticationLoggingFilter;
 import com.github.ciotobe.ssia.chapter9.filter.RequestValidationFilter;
+import com.github.ciotobe.ssia.chapter9.filter.StaticKeyAuthenticationFilter;
 
 @SuppressWarnings("deprecation")
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private StaticKeyAuthenticationFilter filter;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
-		http.addFilterBefore(new RequestValidationFilter(), BasicAuthenticationFilter.class)
-			.addFilterAfter(new AuthenticationLoggingFilter(), BasicAuthenticationFilter.class)
+		http.addFilterAt(filter, BasicAuthenticationFilter.class)
 			.authorizeRequests()
 			.anyRequest()
 			.permitAll();
