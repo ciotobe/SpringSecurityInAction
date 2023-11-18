23bb305262bb1fee5de2f076890473ac4ba2bc84
ciotobe <101615554+ciotobe@users.noreply.github.com>
2023-11-18 22:24:20 +0800

    10.1.2 在实际场景中使用CSRF防护 - 代码清单10.4 - 代码清单10.5 - 代码清单10.6 - 代码清单10.7

--- Chapter10/src/main/java/com/github/ciotobe/ssia/chapter10/config/ProjectConfig.java
+++ Chapter10/src/main/java/com/github/ciotobe/ssia/chapter10/config/ProjectConfig.java
@@ -1,8 +1,14 @@
 package com.github.ciotobe.ssia.chapter10.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CsrfFilter;
 
 import com.github.ciotobe.ssia.chapter10.filter.CsrfTokenLogger;
@@ -10,10 +16,31 @@ import com.github.ciotobe.ssia.chapter10.filter.CsrfTokenLogger;
 @SuppressWarnings("deprecation")
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
+	@Bean
+	public UserDetailsService uds() {
+		var uds = new InMemoryUserDetailsManager();
+		
+		var u1 = User.withUsername("mary")
+					 .password("12345")
+					 .authorities("READ")
+					 .build();
+		
+		uds.createUser(u1);
+		
+		return uds;
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
 	protected void configure(HttpSecurity http) throws Exception{
-		http.addFilterAfter(new CsrfTokenLogger(), CsrfFilter.class)
-			.authorizeHttpRequests()
+		http.authorizeRequests()
 			.anyRequest()
-			.permitAll();
+			.authenticated();
+		
+		http.formLogin()
+			.defaultSuccessUrl("/main", true);
 	}
 }
